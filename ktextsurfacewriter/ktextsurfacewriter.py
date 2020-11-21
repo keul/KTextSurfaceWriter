d19ede615a9d32790464ec039474ed68e38e7b6c
Luca Fabbri <luca@keul.it>
2020-11-21 18:02:02 +0100

    black

--- ktextsurfacewriter/ktextsurfacewriter.py
+++ ktextsurfacewriter/ktextsurfacewriter.py
@@ -12,12 +12,20 @@ LL_OVERFLOW = "overflow"
 PL_OVERFLOW = "overflow"
 PL_CUT = "cut"
 
+
 class KTextSurfaceWriter(object):
     """Generate a text displayed inside a given pygame.Rect.
     You can change/choose font size and color, and can fill the surface part.
     """
-    
-    def __init__(self, rect, font=None, color=(0,0,255,0), fillcolor=(0,0,0,0), justify_chars=0):
+
+    def __init__(
+        self,
+        rect,
+        font=None,
+        color=(0, 0, 255, 0),
+        fillcolor=(0, 0, 0, 0),
+        justify_chars=0,
+    ):
         self.rect = rect
         if not font:
             self.font = pygame.font.Font(None, 16)
@@ -35,12 +43,15 @@ class KTextSurfaceWriter(object):
     def _setText(self, text):
         self._text = text
         self.invalidate()
-    text = property(lambda self: self._text, _setText, doc="""The text to be displayed""")
+
+    text = property(
+        lambda self: self._text, _setText, doc="""The text to be displayed"""
+    )
 
     def invalidate(self):
         """Manually invalidate the text cache"""
         self._resultPage = []
-        self._mustClear = True        
+        self._mustClear = True
 
     @classmethod
     def wordTooLong(cls, word, font, max_length, justify_chars=0):
@@ -52,13 +63,20 @@ class KTextSurfaceWriter(object):
         @return: True if the word will be longer
         """
         # BBB: someday this function could became part of some text (non graphical) utility?
-        return font.size((" "*justify_chars)+word)[0]>max_length
+        return font.size((" " * justify_chars) + word)[0] > max_length
 
     @classmethod
-    def normalizeTextLength(cls, text_too_long, font, max_length, justify_chars=0, line_length_criteria=LL_CUT):
+    def normalizeTextLength(
+        cls,
+        text_too_long,
+        font,
+        max_length,
+        justify_chars=0,
+        line_length_criteria=LL_CUT,
+    ):
         """This function take a text too long and split it in a list of smaller text lines.
         The final text max length must be less/equals than max_length parameter, using the font passed.
-        
+
         @return: a list of text lines.
         """
         # BBB: someday this function could became part of some text (non graphical) utility?
@@ -68,43 +86,63 @@ class KTextSurfaceWriter(object):
         txt1 = txt2 = ""
         while tooLong:
             word = words.pop()
-            if line_length_criteria.lower()==LL_CUT:
+            if line_length_criteria.lower() == LL_CUT:
                 # Simple: cut the word and go on
-                while cls.wordTooLong(word, font, max_length, justify_chars=justify_chars):
+                while cls.wordTooLong(
+                    word, font, max_length, justify_chars=justify_chars
+                ):
                     word = word[:-1].strip()
-            elif line_length_criteria.lower()==LL_SPLIT:
+            elif line_length_criteria.lower() == LL_SPLIT:
                 # Cut the word, re-insert the remaining part as a new word and start again
                 if cls.wordTooLong(word, font, max_length, justify_chars=justify_chars):
                     left_word = word
-                    while cls.wordTooLong(left_word, font, max_length, justify_chars=justify_chars):
+                    while cls.wordTooLong(
+                        left_word, font, max_length, justify_chars=justify_chars
+                    ):
                         left_word = left_word[:-1].strip()
-                    words.extend( [left_word, word[len(left_word):], ] )
+                    words.extend(
+                        [
+                            left_word,
+                            word[len(left_word) :],
+                        ]
+                    )
                     continue
-            elif line_length_criteria.lower()==LL_OVERFLOW:
+            elif line_length_criteria.lower() == LL_OVERFLOW:
                 # Word too long is not changed, so draw outside the defined rect
                 txt1 = " ".join(words)
                 if cls.wordTooLong(word, font, max_length, justify_chars=justify_chars):
                     words_removed.reverse()
-                    txt2 = (" "*justify_chars) + " ".join(words_removed)
-                    if font.size(txt2)[0]<=max_length:
-                        return cls.normalizeTextLength(txt1, font, max_length, justify_chars=justify_chars) + \
-                               [(" "*justify_chars) + word, txt2]
+                    txt2 = (" " * justify_chars) + " ".join(words_removed)
+                    if font.size(txt2)[0] <= max_length:
+                        return cls.normalizeTextLength(
+                            txt1, font, max_length, justify_chars=justify_chars
+                        ) + [(" " * justify_chars) + word, txt2]
                     else:
-                        return cls.normalizeTextLength(txt1, font, max_length, justify_chars=justify_chars) + \
-                               [(" "*justify_chars) + word] + \
-                               cls.normalizeTextLength(txt2, font, max_length, justify_chars=justify_chars)
+                        return (
+                            cls.normalizeTextLength(
+                                txt1, font, max_length, justify_chars=justify_chars
+                            )
+                            + [(" " * justify_chars) + word]
+                            + cls.normalizeTextLength(
+                                txt2, font, max_length, justify_chars=justify_chars
+                            )
+                        )
             else:
-                raise ValueError("Invalid line_length_criteria value: %s" % line_length_criteria)
+                raise ValueError(
+                    "Invalid line_length_criteria value: %s" % line_length_criteria
+                )
             words_removed.append(word)
             txt1 = " ".join(words)
-            if font.size(txt1)[0]<=max_length:
+            if font.size(txt1)[0] <= max_length:
                 tooLong = False
         words_removed.reverse()
-        txt2 = (" "*justify_chars) + " ".join(words_removed)
-        if font.size(txt2)[0]<=max_length:
+        txt2 = (" " * justify_chars) + " ".join(words_removed)
+        if font.size(txt2)[0] <= max_length:
             return [txt1, txt2]
         else:
-            return [txt1] + cls.normalizeTextLength(txt2, font, max_length, justify_chars=justify_chars)
+            return [txt1] + cls.normalizeTextLength(
+                txt2, font, max_length, justify_chars=justify_chars
+            )
 
     def _getPreparedText(self):
         """Prepare text for future rendering.
@@ -118,16 +156,20 @@ class KTextSurfaceWriter(object):
         resultPage = []
         for line in text.split("\n"):
             lw, lh = self.font.size(line)
-            if lw>rw:
-                newtextlines = self.normalizeTextLength(line,
-                                                        self.font,
-                                                        rw,
-                                                        justify_chars=self.justify_chars,
-                                                        line_length_criteria=self.line_length_criteria)
+            if lw > rw:
+                newtextlines = self.normalizeTextLength(
+                    line,
+                    self.font,
+                    rw,
+                    justify_chars=self.justify_chars,
+                    line_length_criteria=self.line_length_criteria,
+                )
             else:
-                newtextlines = [line,]
+                newtextlines = [
+                    line,
+                ]
             resultPage.extend(newtextlines)
-        if self.page_length_criteria==PL_CUT:
+        if self.page_length_criteria == PL_CUT:
             resultPage = self._shortDownPage(resultPage)
         self._resultPage = resultPage
         return resultPage
@@ -141,7 +183,7 @@ class KTextSurfaceWriter(object):
         @return: the page parameter without the not needed lines.
         """
         ln = len(page)
-        while ln*self.font.get_height()>self.rect.height:
+        while ln * self.font.get_height() > self.rect.height:
             page.pop()
             ln = len(page)
         return page
@@ -163,8 +205,8 @@ class KTextSurfaceWriter(object):
         i = 0
         for line in resultPage:
             ren = self.font.render(line, 1, self.color, self.fillcolor)
-            surface.blit(ren, (rect.left, rect.top + i*self.font.get_height()))
-            i+=1
+            surface.blit(ren, (rect.left, rect.top + i * self.font.get_height()))
+            i += 1
 
 
 def runTests():
